009879f0637dfb1f6a3d8c001f10e26c5f021d58
Nuanu-123 <89157371+Nuanu-123@users.noreply.github.com>
2021-11-05 13:36:30 +0000

    Add files via upload

---
+++ Profile/EducationProfile.cs
@@ -0,0 +1,62 @@
+﻿using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace MarsQA_1.SpecflowPages.Pages
+{
+    public class EducationProfile
+    {
+        public string Country { get; set; }
+        public string University { get; set; }
+        public string Title { get; set; }
+        public string Degree { get; set; }
+        public int YearOfGraduation { get; set; }
+        public string EducationMessage { get; set; }
+
+        IWebElement EducationTab => Driver.driver.FindElement(By.XPath("(//A[text()='Education'])[1]"));
+        IWebElement AddNewButton => Driver.driver.FindElement(By.XPath("(//div[text()='Add New'])[3]"));
+        IWebElement UniversityName => Driver.driver.FindElement(By.XPath("//INPUT[@placeholder='College/University Name']"));
+        IWebElement CountryName => Driver.driver.FindElement(By.XPath("//SELECT[@class='ui dropdown'][@name='country']"));
+        IWebElement TitleDropDown => Driver.driver.FindElement(By.XPath("//SELECT[@class='ui dropdown'][@name='title']"));
+        IWebElement DegreeName => Driver.driver.FindElement(By.XPath("//INPUT[@placeholder='Degree']"));
+        IWebElement GraduationYear => Driver.driver.FindElement(By.XPath("//SELECT[@class='ui dropdown'][@name='yearOfGraduation']"));
+        IWebElement AddButton => Driver.driver.FindElement(By.XPath("(//input[@type='button'][@value='Add'])"));
+        string VerifyAction = "//div[@class='ns-box-inner']";
+        IWebElement AlertBoxMessage => Driver.driver.FindElement(By.XPath(VerifyAction));
+
+        public void AddEducation()
+        {
+            EducationTab.Click();
+            AddNewButton.Click();
+            UniversityName.SendKeys(University);
+            //select country name 
+            var SelectCountry = new SelectElement(CountryName);
+            SelectCountry.SelectByText(Country);
+            //select title 
+            var SelectTitle = new SelectElement(TitleDropDown);
+            SelectTitle.SelectByText(Title);
+            DegreeName.SendKeys(Degree);
+            //select graduation year 
+            var SelectGradYear = new SelectElement(GraduationYear);
+            SelectGradYear.SelectByText(YearOfGraduation.ToString());
+            AddButton.Click();
+            Thread.Sleep(2000);
+            EducationMessage = AlertBoxMessage.Text;
+        }
+
+        // Delete added record
+        public void DeleteEducation()
+
+        {
+            EducationTab.Click();
+            var data = Title;
+            Thread.Sleep(2000);
+            IWebElement EduDeleteButton = Driver.driver.FindElement(By.XPath("//td[text()='" + data + "']//following::td[3]//descendant::i[@class='remove icon']"));
+            EduDeleteButton.Click();
+            Thread.Sleep(2000);
+            EducationMessage = AlertBoxMessage.Text;
+        }
+    }
+}
+
---
+++ Profile/SkillProfile.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading;
+using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace MarsQA_1.Pages
+{
+    public class SkillProfile
+    {
+        public string Skill { get; set; }
+        public string Level { get; set; }
+        public string SkillMessage { get; set; }
+IWebElement SkillTab => Driver.driver.FindElement(By.XPath("//A[text()='Skills']"));
+        IWebElement AddNewButton => Driver.driver.FindElement(By.XPath("//div[@class='ui teal button'][text()='Add New']"));
+        IWebElement SkillTextField => Driver.driver.FindElement(By.XPath("//INPUT[@placeholder='Add Skill']"));
+        IWebElement SelectLevel => Driver.driver.FindElement(By.XPath("//SELECT[@class='ui fluid dropdown'][@name='level']"));
+        IWebElement AddButton => Driver.driver.FindElement(By.XPath("(//input[@type = 'button'][@value = 'Add'])"));
+        string VerifyAction = "//div[@class='ns-box-inner']";
+        IWebElement UpdateBtn => Driver.driver.FindElement(By.XPath("(//input[@type = 'button'][@value = 'Update'])"));
+        IWebElement AlertBoxMessage => Driver.driver.FindElement(By.XPath(VerifyAction));
+        public void AddSkill()
+        {
+            SkillTab.Click();
+            AddNewButton.Click();
+            SkillTextField.SendKeys(Skill);
+            var selectElement = new SelectElement(SelectLevel);
+            selectElement.SelectByText(Level);
+            AddButton.Click();
+            Thread.Sleep(2000);
+            SkillMessage = AlertBoxMessage.Text;
+        }
+        //Update
+        public void UpdateSkill()
+        {
+            SkillTab.Click();
+            var data = Skill;
+            IWebElement EditButtonPen = Driver.driver.FindElement(By.XPath("//td[text()='" + data + "']/following::td[2]/descendant::i[@class='outline write icon']"));
+            EditButtonPen.Click();
+            SkillTextField.Clear();
+            SkillTextField.SendKeys(Skill);
+            var selectElement = new SelectElement(SelectLevel);
+            selectElement.SelectByText(Level);
+            UpdateBtn.Click();
+            Thread.Sleep(2000);
+            SkillMessage = AlertBoxMessage.Text;
+        }
+        // Delete an updated record
+        public void DeleteSkill()
+        {
+            SkillTab.Click();
+            var data1 = Skill;
+            Thread.Sleep(2000);
+            IWebElement ClearButtonPen = Driver.driver.FindElement(By.XPath("//td[text()='" + data1 + "']//following::td[2]//descendant::i[@class='remove icon']"));
+            ClearButtonPen.Click();
+            Thread.Sleep(2000);
+            SkillMessage = AlertBoxMessage.Text;
+
+        }
+    }
+}
+
